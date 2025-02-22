33022ddf71de03edafad6232437c482d64ae68ef
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-02-22 22:28:22 +0000

    change

--- public/js/geo.js
+++ public/js/geo.js
@@ -1,9 +1,6 @@
 // The API Key
 const myAPIKey = "58bbe2d10382465d81736a583252cd67";
 
-userlat = 0;
-userlng = 0;
-
 function getLocation(){
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(appPosition);
@@ -15,7 +12,22 @@ function appPosition(position){
     userlat = position.coords.latitude;
     userlng = position.coords.longitude;
 
-    return userlat, userlng;
+    const reverseGeocodingUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${userlat}&lon=${userlng}&apiKey=${myAPIKey}`;
+
+    // call Reverse Geocoding API - https://www.geoapify.com/reverse-geocoding-api/
+    fetch(reverseGeocodingUrl).then(result => result.json())
+    .then(featureCollection => {
+    if (featureCollection.features.length === 0) {
+        console.log("Address Not Found");
+        return;
+    }
+
+    const foundAddress = featureCollection.features[0];
+    console.log(foundAddress.properties.city);
+    console.log(foundAddress.properties.country);
+
+    });
+
 }
 
 //console.log(userlat, userlng);
