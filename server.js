21894f7f4ef7d348dfb8fa8778fa0077d159e098
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-02-23 08:31:01 +0000

    test

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/datapage', function(req, res){
 app.post('/search', function(req, res){
     console.log(req.body.searchWild);
     searchSpecies = req.body.searchWild;
-    db.wildlife.find({"species":searchSpecies}, function(err, result){
+    db.collection("wildlife").findOne({"species":searchSpecies}, function(err, result){
         if (err) throw err;
         res.render('pages/datapage', {
             searchResult: result
