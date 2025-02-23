c5765edbee5ca4219cbb92f310d3c79c4f6a8359
Matthew <75327868+matthlmes@users.noreply.github.com>
2025-02-23 08:08:55 +0000

    test

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.post('/search', function(req, res){
         res.render('pages/datapage', {
             searchResult: result
         })
-        document.getElementById("searchResult").innerHTML = result;
+        console.log(result);
 
     });
 
